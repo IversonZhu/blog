a440f0999a2eda2d4055d42dad781fb820eab18d
Iverson <zhuhuayao_2014@163.com>
2019-08-18 23:51:58 +0800

    修改-->安全权限配置

--- src/main/java/com/iverson/blog/config/SecurityConfig.java
+++ src/main/java/com/iverson/blog/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static  final String KEY = "SECURITY_SYSTEM";
+
     /**
      * 拦截配置
      * @param http
@@ -23,9 +25,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/css/**","/js/**","/fonts/**","/index").permitAll()//均可访问
-            .antMatchers("/user/**").hasRole("admin")//需要相应的角色才可以访问
+            .antMatchers("/h2-console/**").permitAll()//均可访问
+            .antMatchers("/admins/**").hasRole("admin")//需要相应的角色才可以访问
             .and()
             .formLogin()//基于form表单登陆验证
-            .loginPage("/login").failureUrl("/login-error");//跳转登录页面
+            .loginPage("/login").failureUrl("/login-error")
+            .and().rememberMe().key(KEY)//启用RememberMe模式
+            .and().exceptionHandling().accessDeniedPage("/403");//处理异常，拒绝访问就重定向到403页面
+        http.csrf().ignoringAntMatchers("/h2-console/**");//禁用H2控制台的CSRF防护
+        http.headers().frameOptions().sameOrigin();//允许来自同一来源的H2控制的请求
     }
+
+    
 }
